53a46a8493f44783e9d58ccf847bccdac6dde255
Ubuntu <ubuntu@ip-10-0-0-155.(none)>
2016-01-10 10:08:00 +0000

    clearing snapshots, taking snapshot of all keyspaces, and actually working

--- cassandra-backup.sh
+++ cassandra-backup.sh
@@ -9,26 +9,26 @@ SNAPSHOTID=`uuidgen --time`
 #PGP_KEY_RECIPIENT=YOUR-PGP-KEY-RECIPIENT
 
 echo "------ NEW RUN ------"
+echo "Clearing existing snapshots"
+nodetool clearsnapshot
+
 echo "Taking daily db dump for $DATE with id=$SNAPSHOTID"
 
-#nodetool snapshot --tag $SNAPSHOTID $KEYSPACE
+nodetool snapshot --tag $SNAPSHOTID
 
 TABLES=`ls /mnt/cassandra/data`
 for table in $TABLES
 do
     echo ""
     echo "Encrypting and sending all $table files one by one..."
-    #FILES=`find /mnt/cassandra/data/$KEYSPACE/${table}/snapshots/$SNAPSHOTID -type f`
     FILES=`find /mnt/cassandra/data/${table}/*/snapshots/$SNAPSHOTID -type f`
     for filename in $FILES
     do
         # Need to deal with files one by one to not have the file usage explode
         # because we are dealing with hardlinks.
-        #aws s3 cp $filename $S3_BUCKET/$S3_BUCKET_PATH/$table/`basename $filename` 
+        aws s3 cp $filename $S3_BUCKET/$S3_BUCKET_PATH/$table/`basename $filename` 
         echo 'Current file is - '$filename
     done
-
-    #rmdir -v /var/lib/cassandra/data/$KEYSPACE/${table}/snapshots/$SNAPSHOTID
 done
 
 echo "DONE!"
